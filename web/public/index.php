9a65d849c1d12c0fbdcaf307db812942309bae6e
Tychcl <tocabloha@gmail.com>
2025-10-08 15:33:21 +0500

    Теперь никто не пройдет исправил ошибки аунтефикации

--- web/public/index.php
+++ web/public/index.php
@@ -3,6 +3,7 @@ session_start();
 use Core\Request;
 use Middleware\AuthMiddleware;
 use Classes\Render;
+use Core\JWToken;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/propel/generated/conf/config.php';
@@ -11,11 +12,15 @@ require_once dirname(__DIR__) . '/propel/generated/conf/config.php';
 //
 ////echo trim('///user/signin', '/');
 //return;
-if(AuthMiddleware::isAuthenticated($_SESSION)){
-    //echo json_encode($_SESSION);
-    echo Render::renderTemplate(null,[], 'main.php');
-}else{
-    //echo json_encode($_SESSION);
+
+if(!$_COOKIE['jwt']){
     echo Render::renderTemplate('user/signin');
+    return;
 }
+$_SESSION = JWToken::validateToken($_COOKIE['jwt']) ?? null;
+if(empty($_SESSION)){
+    echo Render::renderTemplate('user/signin');
+    return;
+}
+echo Render::renderTemplate(null,[], 'main.php');
 ?>
--- web/src/core/middleware/AuthMiddleware.php
+++ web/src/core/middleware/AuthMiddleware.php
@@ -28,10 +28,11 @@ class AuthMiddleware implements MiddlewareInterface
         if($request->cookie['jwt'] && empty($_SESSION))
         {
             $_SESSION = JWToken::validateToken($request->cookie['jwt']) ?? null;;
-        }
-        if (!in_array($uri, $this->exception) && !$this->isAuthenticated($_SESSION)) {
+        }elseif(!$request->cookie['jwt']){
             session_destroy();
             $_SESSION = array();
+        }
+        if (!in_array($uri, $this->exception) && !$this->isAuthenticated($_SESSION)) {
             return new Response(401, ['error'=>'Unauthorized']);
         }
         if(in_array($uri, $this->admin) && $_SESSION['roleid'] != 1){
